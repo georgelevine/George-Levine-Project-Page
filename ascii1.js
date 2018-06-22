26da854d6a12b44140571c5a7d8c8f2bc8a3bd1b
George Levine <george@georgelevine.com>
2018-06-22 11:57:50 -0400

    update

--- ascii1.js
+++ ascii1.js
@@ -1,17 +1,22 @@
 var x= "";
+var k = 0;
+var pictures;
+var time = 0;
 
 window.onload = function() {
 document.getElementById("startButton").onclick=showAnimation;
 document.getElementById("animation").onchange = changeAnimation;
 document.getElementById('size').onchange=sizeAnimation;
-
+document.getElementById('stopButton').onclick = abortAnimation;
 }
 function showAnimation(){
   var textarea=document.getElementById('mytextarea');
 //  textarea.innerHTML=ANIMATIONS["Exercise"] + ANIMATIONS["Juggler"];
-    var pictures = ANIMATIONS[x].split("=====\n");
-    textarea.innerHTML=pictures[0];
-      textarea.innerHTML=pictures[1];
+     pictures = ANIMATIONS[x].split("=====\n");
+
+    //  textarea.innerHTML=pictures[1];
+    time = setInterval(increaseIndex, 300);
+    textarea.innerHTML=pictures[k];
 }
 function changeAnimation(){
    x = document.getElementById("animation").value;
@@ -23,4 +28,16 @@ function sizeAnimation() {
   console.log(textSize);
   var textArea=document.getElementById('mytextarea');
   textArea.style.fontSize=textSize;
+}
+function increaseIndex(){
+   k = (k + 1) % pictures.length;
+var textarea=document.getElementById('mytextarea');
+textarea.innerHTML=pictures[k];
+   console.log(pictures.length);
+   console.log(k);
+
+}
+function abortAnimation() {
+  clearInterval(time);
+
 }
