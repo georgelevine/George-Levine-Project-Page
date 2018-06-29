4ef9ebd27689f9e0d31ed0fff565b86d0711c31a
George Levine <george@georgelevine.com>
2018-06-29 18:54:15 -0400

    George Levine Key JavaScript Concepts

--- fleetList.js
+++
@@ -1,5 +0,0 @@
-var VEHICLES = "  ford, 1234, Ana\n" +
-    " toyota, 543, Bob \n" +
-      " toyota, 5413, Lil \n" +
-        " toyota, 943, Mike \n" +
-    " ford, 9876, Tom\n" ;
---
+++ js1.js
@@ -0,0 +1,15 @@
+function simpleAlert() {
+alert("You Pressed It!");
+}
+function harderAlert() {
+alert("You Found It");
+}
+function textSwap() {
+  var paragraph = document.getElementById("textArea1");
+  paragraph.innerHTML = "Hover Text";
+}
+function textReset() {
+  var paragraph = document.getElementById("textArea1");
+  paragraph.innerHTML = "Original Text";
+
+}
